5dd3337da42ddaa850692f9efc1d79192c3d6443
Udit Padhan <uditpadhan98@gmail.com>
2023-04-05 15:24:07 +0530

    Landing Page Done

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,14 +2,31 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { Button } from '@mui/material';
 import '../LandingPage/LandingPage.css'
+import LandingImg from '../../images/Landing-img.jpg';
 
 export default function LandingPage() {
   return (
       <>
         <div className='Landing-container'>
-          <h1>Sparsh</h1>
-          <h5>"Your health is our top priority - let us guide you on your journey to wellness."</h5>
-          <NavLink className='NavLink' to="/SignIn"><Button variant="contained">Sign in</Button></NavLink> 
+        <img className='LandingImg' src={LandingImg} alt="udit"></img>
+          <div>
+            <div className='Landing-Navbar'>
+              <ul>
+                <NavLink className='Landing-Nav' to='/Home'>About</NavLink>
+                <NavLink className='Landing-Nav' to='/Appointment'>Appointment</NavLink>
+                <NavLink className='Landing-Nav' to='/BMI'>BMI</NavLink>
+                <NavLink className='Landing-Nav' to='/BloodDonation'>Blood-Donation</NavLink>
+                <NavLink className='Landing-Nav' to='/ContactUs'>Contact-Us</NavLink>
+              </ul>
+            </div>
+            <div className='Landing-content'>
+              <h1>Sparsh</h1>
+              <br />
+              <h5>"Your health is our top priority - "</h5>
+              <h5>let us guide you on your journey to wellness.</h5>
+              <NavLink className='NavLink' to="/SignIn"><Button variant="contained">Sign in</Button></NavLink> 
+            </div>
+          </div>
         </div>
       </>
   );
--- src/components/Navbar/Router.js
+++ src/components/Navbar/Router.js
@@ -3,6 +3,10 @@ import LandingPage from "../LandingPage/LandingPage";
 import SignInPage from '../SignInPage/SignIn';
 import SignUp from '../SignUpPage/SignUp';
 import Home from '../HomePage/Home';
+import Appointment from '../Content/Appointment';
+import BloodDonation from '../Content/BloodDonation';
+import BMI from '../Content/BMI';
+import ContactUs from '../Content/ContactUs';
 
 function Router() {
   return (
@@ -11,6 +15,10 @@ function Router() {
           <Route path="/SignIn" element={<SignInPage/>} />
           <Route path="/SignUp" element={<SignUp/>} />
           <Route path="/Home" element={<Home/>}/>
+          <Route path="/Appointment" element={<Appointment/>} />
+          <Route path="/BloodDonation" element={<BloodDonation/>} />
+          <Route path="/BMI" element={<BMI/>} />
+          <Route path="/ContactUs" element={<ContactUs/>}/>
         </Routes>  
   );
 }
