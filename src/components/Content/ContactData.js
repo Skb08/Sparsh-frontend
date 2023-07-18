a0c670a05df40d0cb6fde91666f847c43478da3d
suraj <surajkumarbehera110@gmail.com>
2023-07-19 00:46:14 +0530

    landing page completed

--- src/components/Content/ContactData.js
+++ src/components/Content/ContactData.js
@@ -1,28 +1,25 @@
-import BloodImg from '../../images/blood-img.png'
-import signInimg from '../../images/signin-image.jpg'
-
 export const ContactData=[
     {
         name:"Naveen",
-        image:BloodImg,
+        image:"https://img.freepik.com/premium-photo/cute-boy-pixar-style-cartoon-3d-illustration-generative-ai_808510-251.jpg",
         reciever1:"https://www.instagram.com/yo_u_dit/",
         reciever2:"https://www.linkedin.com/in/udit-padhan-41481a222/"
     },
     {
         name:"Prayant",
-        image:signInimg,
+        image:"https://img.freepik.com/premium-photo/small-boy-colorful-background-funny-cartoon-character-school-kid-3d-generative-ai_58409-28606.jpg?w=2000",
         reciever1:"https://www.instagram.com/yo_u_dit/",
         reciever2:"https://www.linkedin.com/in/udit-padhan-41481a222/"
     },
     {
         name:"Suraj Ku. Behera",
-        image:signInimg,
+        image:"https://img.freepik.com/premium-photo/small-boy-colorful-background-funny-cartoon-character-school-kid-3d-generative-ai_58409-28529.jpg?w=2000",
         reciever1:"https://www.instagram.com/yo_u_dit/",
         reciever2:"https://www.linkedin.com/in/udit-padhan-41481a222/"
     },
     {
         name:"Udit Padhan",
-        image:BloodImg,
+        image:"https://img.freepik.com/premium-photo/cute-boy-pixar-style-cartoon-3d-illustration-generative-ai_808510-252.jpg",
         reciever1:"https://www.instagram.com/yo_u_dit/",
         reciever2:"https://www.linkedin.com/in/udit-padhan-41481a222/"
     },
